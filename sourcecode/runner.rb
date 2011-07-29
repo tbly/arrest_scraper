8e8f62d3f46b0d77d6cdad305b245504983b9015
Ly Tieu Bang <lytieubang@yahoo.com>
2011-07-29 14:58:14 +0700

    update runner.rb script

--- sourcecode/runner.rb
+++ sourcecode/runner.rb
@@ -1,7 +1,6 @@
 require 'rubygems'
 require 'arrest_scraper'
 require 'date'
-require 'fakeweb'
 
 # usage
 # ruby runner.rb [-n|--number-of-days number-of-days]
@@ -11,12 +10,6 @@ n = ARGV[ARGV.index(n) + 1].to_i rescue 0
 
 arrest_scraper = ArrestScraper.new
 results = arrest_scraper.process(n,Date.today,'http://www.iowa-city.org/icgov/apps/police/blotter.asp')
-
-# return single ArrestDay object that contains an array of Arrest objects
-puts results.collect{|r| "Date: #{r.date.strftime('%m/%d/%Y')} Arrests: #{r.arrests.size}"}.join("\n")
-puts "DONE"
-
-results = arrest_scraper.process(n,Date.new(2011,7,1),'http://www.iowa-city.org/icgov/apps/police/blotter.asp')
 
 # return single ArrestDay object that contains an array of Arrest objects
 puts results.collect{|r| "Date: #{r.date.strftime('%m/%d/%Y')} Arrests: #{r.arrests.size}"}.join("\n")
